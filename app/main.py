a8803062f13df4fe6ed6a53f69009cde3ce91632
Po33ski <jaroslaw.popardowski@wp.pl>
2026-04-22 16:39:00 +0200

    Solution2

--- app/main.py
+++ app/main.py
@@ -13,11 +13,12 @@ class Person:
 
 def create_person_list(people: list) -> list:
     person_class_list = list(map(lambda person_dict: Person(name=person_dict["name"], age=person_dict["age"]), people))
-    for person in people:
-        if person.get("wife") is not None:
-            person_class_list[people.index(person)].wife = person_class_list[people.index(next(filter(lambda p: p["name"] == person["wife"], people)))]
-        elif person.get("husband") is not None:
-            person_class_list[people.index(person)].husband = person_class_list[people.index(next(filter(lambda p: p["name"] == person["husband"], people)))]
+    for person_dict in people:
+        person_instance = Person.people[person_dict["name"]]
+        if person_dict.get("wife"):
+            person_instance.wife = Person.people[person_dict["wife"]]
+        if person_dict.get("husband"):
+            person_instance.husband = Person.people[person_dict["husband"]]
     return person_class_list
 
 
