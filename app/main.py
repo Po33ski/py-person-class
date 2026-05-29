add45faa7a463a13a0538aec828799648a1a0493
Po33ski <jaroslaw.popardowski@wp.pl>
2026-05-29 16:23:15 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,24 +1,24 @@
 class Person:
-    people = {}
-
+    people = dict()
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
         Person.people[self.name] = self
+        self.wife = None
+        self.husband = None
+
+    def greet(self) -> str:
+        return f"Hello, my name is {self.name} and I am {self.age} years old."
 
 
 def create_person_list(people: list[dict]) -> list:
     person_instances = [
         Person(person["name"], person["age"]) for person in people
     ]
-    for person in people:
-        if person.get("wife"):
-            Person.people[person["name"]].wife = (
-                Person.people[person["wife"]]
-            )
-        elif person.get("husband"):
-            Person.people[person["name"]].husband = (
-                Person.people[person["husband"]]
-            )
+    for person_dict in people:
+        person_instance = Person.people[person_dict["name"]]
+        if person_dict.get("wife"):
+            person_instance.wife = Person.people[person_dict["wife"]]
+        if person_dict.get("husband"):
+            person_instance.husband = Person.people[person_dict["husband"]]
     return person_instances
-
